0f082eeeab024664269a5d71ba9f8dedb338f687
Igor <marcikicigor@gmail.com>
2020-02-22 13:43:26 +0100

    Added routers and stylization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import Nav from './components/Nav';
 import Posts from './Pages/Posts';
+import About from './Pages/About';
 import Provider from './Context/Context';
 import * as styles from './App.module.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className= {styles.App}>
-      <h1>Hacker News</h1>
-      <Provider>
-        <Posts />
-      </Provider>
+    <div className={styles.App}>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Provider>
+              <Posts />
+            </Provider>
+          </Route>
+          <Route exact path="/about" component={About}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as styles from './Nav.module.scss';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <>
+            <nav>
+                <div className={styles.Logo}><Link to="/">Hacker News</Link></div>
+                <div className={styles.item}><Link to="/">Posts</Link></div>
+                <div className={styles.item}><Link to="/about">About</Link></div>
+            </nav>
+        </>
+    )
+}
+
+export default Nav;
